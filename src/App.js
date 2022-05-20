1133afb69354d05189b25479c596feb4daf5ee63
Raju Chandnani <Raju.Chandnani@silkroad.com>
2022-05-20 09:03:15 -0700

    abc

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     async componentDidMount() {
         const response = await fetch(
       //    "https://bpvm2g9ut4.execute-api.us-east-1.amazonaws.com/DEV/", 
-          https://xex1eec4md.execute-api.us-east-1.amazonaws.com/development/employees,
+         "https://xex1eec4md.execute-api.us-east-1.amazonaws.com/development/employees/",
 	  {
            headers: new Headers({'Authorization': 'allow'})    
         }    
